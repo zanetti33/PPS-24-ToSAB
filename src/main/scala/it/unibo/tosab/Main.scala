9e860efff8eb85b35ef8b08a26082f80b9b0ed98
lzanetti <lorenzo.zanetti5@studio.unibo.it>
2026-03-24 11:31:18 +0100

    feat: initial bootstrap main

--- src/main/scala/it/unibo/tosab/Main.scala
+++ src/main/scala/it/unibo/tosab/Main.scala
@@ -1,8 +1,41 @@
 package it.unibo.tosab
 
-object Main {
-  def main(args: Array[String]): Unit = {
-    val hello = new Hello()
-    println(hello.greet())
-  }
-}
+object Main:
+
+  /**
+   * Bootstrap main for sprint 1
+   * @param args
+   */
+  def main(args: Array[String]): Unit =
+    println("==============================")
+    println(" TOWN OF SAVIOM: AUTO-BATTLER ")
+    println("==============================")
+
+    println("[INIT] Generazione Griglia 10x10...")
+    val initialGrid = Grid(10, 10)
+
+    println("[INIT] Creazione Unità in corso...")
+    // Creiamo alcune unità di prova usando le strutture immutabili
+    val u1 = Archer()
+    val u2 = Soldier()
+    val u3 = Mage()
+
+    println("[INIT] Inizializzazione GameState globale...")
+    val initialState = GameState(initialGrid)
+
+    initialState.setUnit(u1, Coordinate(0, 2))
+    initialState.setUnit(u2, Coordinate(3, 2))
+    initialState.setUnit(u3, Coordinate(4, 5))
+
+    println("\n[STATO GIOCO SPRINT 1] Setup completato con successo!\n")
+
+    renderTerminalView(initialState)
+
+  private def renderTerminalView(state: GameState): Unit =
+    println(s"--- FASE ATTUALE: ${} ---")
+    println(s"Mappa: ${}x${}")
+    println("Unità in campo:")
+
+    // TODO access units and simple print
+
+    println("=========================================\n")
