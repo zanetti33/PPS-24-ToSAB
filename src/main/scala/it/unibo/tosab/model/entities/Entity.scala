9dd2fd16b1e187b9a3bfee4c2a97ba7bdaff7dad
Fabio Pedrini <fabio.pedrini3@studio.unibo.it>
2026-03-31 09:44:37 +0200

    feat: implemented Entity type

--- src/main/scala/it/unibo/tosab/model/entities/Entity.scala
+++ src/main/scala/it/unibo/tosab/model/entities/Entity.scala
@@ -1,3 +1,32 @@
 package it.unibo.tosab.model.entities
 
-case class Entity(value: Int)
+import it.unibo.tosab.model.entities.Role.{Archer, Mage, Soldier}
+import it.unibo.tosab.model.entities.Stats.*
+
+enum Role:
+  case Archer, Soldier, Mage
+
+trait Entity:
+  def id: String
+  def role: Role
+  def stats: Stats
+
+  def takeDamage(amount: Int): Entity
+
+case class Character(id: String, role: Role, stats: Stats) extends Entity:
+
+  def takeDamage(amount: Int): Entity =
+    val newHp = Math.max(0, stats.currentHp - amount)
+    val newStats = stats.copy(currentHp = newHp)
+    this.copy(stats = newStats)
+
+object Entity:
+
+  def createArcher(id: String): Entity =
+    Character(id, Archer, baseArcherStats)
+
+  def createSoldier(id: String): Entity =
+    Character(id, Soldier, stats = baseSoldierStats)
+
+  def createMage(id: String): Entity =
+    Character(id, Mage, stats = baseMageStats)
--- src/test/scala/it/unibo/tosab/model/entities/EntityTest.scala
+++ src/test/scala/it/unibo/tosab/model/entities/EntityTest.scala
@@ -2,11 +2,32 @@ package it.unibo.tosab.model.entities
 
 import org.junit.Test
 import org.junit.Assert.*
-import it.unibo.tosab.model.entities.Entity
+import it.unibo.tosab.model.entities.Entity.*
 
 class EntityTest:
 
+  val soldier: Entity = createSoldier("s1")
+  val archer: Entity = createArcher("a1")
+  val mage: Entity = createMage("m1")
+
   @Test def entityExistTest(): Unit =
-    val expectedValue = 1
-    val e1 = Entity(expectedValue)
-    assertEquals(expectedValue, e1.value)
+    assertNotNull(soldier)
+
+  @Test def entityIsASoldier(): Unit =
+    assertEquals(Role.Soldier, soldier.role)
+
+  @Test def entityIsAnArcher(): Unit =
+    assertEquals(Role.Archer, archer.role)
+
+  @Test def entityIsAMage(): Unit =
+    assertEquals(Role.Mage, mage.role)
+
+  @Test def soldierHasRightHP(): Unit =
+    assertEquals(50, soldier.stats.currentHp)
+
+  @Test def soldierHasRightAttackType(): Unit =
+    assertEquals("Melee", soldier.stats.attackType)
+
+  @Test def entityTakesDamage(): Unit =
+    val damagedSoldier = soldier.takeDamage(20)
+    assertEquals(30, damagedSoldier.stats.currentHp) // 50 - 20 = 30
