95ad331c1216b3372af1a2209c9a8d8db3f4c017
lzanetti <lorenzo.zanetti5@studio.unibo.it>
2026-04-03 13:05:04 +0200

    test: DummyEngine does nothing

---
+++ src/test/scala/it/unibo/tosab/model/engine/EngineTest.scala
@@ -0,0 +1,23 @@
+package it.unibo.tosab.model.engine
+
+import org.junit.*
+import org.junit.Assert.*
+import it.unibo.tosab.model.GameAction
+import it.unibo.tosab.model.GameState.{ GamePhase, GameState }
+import it.unibo.tosab.model.engine.Engine.{ DummyEngine, Engine }
+import it.unibo.tosab.model.grid.Grid
+
+class EngineTest:
+
+  val grid: Grid       = Grid()
+  val combatState: GameState  = GameState(GamePhase.Combat, grid)
+
+  @Test def dummyEngineDoesNotChangePhase(): Unit =
+    val engine: Engine = DummyEngine
+    val result = engine.applyAction(combatState, GameAction.Pass)
+    assertEquals(combatState, result)
+
+  @Test def dummyEngineDoesNotChangeGrid(): Unit =
+    grid.setCell("archer", (1, 1))
+    val result = DummyEngine.applyAction(combatState, GameAction.Pass)
+    assertEquals("archer", result.grid.getEntity((1, 1)))
