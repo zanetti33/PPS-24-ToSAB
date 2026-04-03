54eb8bae0af74d996a882ed15667a332a5211469
Alessandro Zanzi <alessandro.zanzi2@studio.unibo.it>
2026-04-03 17:28:27 +0200

    feature: insert the positioning of the Entities and the check based on their faction. Updated the test changing the input from string to Entity

--- src/main/scala/it/unibo/tosab/Main.scala
+++ src/main/scala/it/unibo/tosab/Main.scala
@@ -1,11 +1,21 @@
 package it.unibo.tosab
 import it.unibo.tosab.model.grid.*
+import it.unibo.tosab.model.entities.*
+import it.unibo.tosab.model.entities.Entity.*
+import it.unibo.tosab.view.DisplayGrid
 
 @main def runApp(): Unit =
   val grid = Grid()
-  grid.setCell("archer", (3, 5))
-  grid.setCell("wizard", (6, 4))
-  grid.setCell("soldier", (8, 8))
+  val wizard = createEntity("wizard", Faction.Player, Role.Mage)
+  val soldier = createEntity("soldier", Faction.AI, Role.Soldier)
+  val soldier2 = createEntity("soldier", Faction.AI, Role.Soldier)
+  grid.setCell(wizard, (5, 5))
+  grid.setCell(soldier, (1, 4))
+  grid.setCell(soldier, (8, 8))
+  grid.setCell(soldier, (2, 4))
+  grid.setCell(soldier2, (3, 4))
+  // da controllare numerazione truppe sia se ne creo due uguali che se ne creo due diversi,
+  // se è possibile creare due truppe uguali e se è possibile creare due truppe diverse con lo stesso id
   DisplayGrid.display(grid)
 
   /*
--- src/test/scala/it/unibo/tosab/model/grid/GridTest.scala
+++ src/test/scala/it/unibo/tosab/model/grid/GridTest.scala
@@ -1,4 +1,6 @@
 package it.unibo.tosab.model.grid
+import it.unibo.tosab.model.entities.Entity.createEntity
+import it.unibo.tosab.model.entities.{Faction, Role}
 import org.junit.*
 import org.junit.Assert.*
 
@@ -8,35 +10,42 @@ class GridTest:
   val grid = Grid()
 
   @Test def testUnitPosition(): Unit =
-    grid.setCell("soldier", (3, 3))
-    assertEquals("soldier", grid.getEntity((3, 3)))
+    val wizard = createEntity("wizard", Faction.AI, Role.Mage)
+    grid.setCell(wizard, (3, 3))
+    assertEquals(Some(wizard), grid.getEntity((3, 3)))
 
   @Test def testInvalidCell(): Unit =
-    grid.setCell("soldier", (8, 8))
+    val soldier = createEntity("soldier", Faction.AI, Role.Soldier)
+    grid.setCell(soldier, (8, 8))
     val occupiedCells = grid.getOccupiedCells
     assertFalse(occupiedCells.contains((8, 8)))
 
   @Test def testGetOccupiedCells(): Unit =
-    grid.setCell("soldier", (1, 1))
+    val soldier = createEntity("soldier", Faction.AI, Role.Soldier)
+    grid.setCell(soldier, (1, 1))
     val occupiedCells = grid.getOccupiedCells
     assertTrue(occupiedCells.contains((1, 1)))
     assertEquals(1, occupiedCells.size)
 
   @Test def testGetAdjacentCellsEvenRow(): Unit =
-    grid.setCell("soldier", (2, 1))
-    val availableCellsSoldier1 = grid.getAdjacentAvailableCells("soldier")
-    val expected1 = Set((1, 0), (1, 1), (2, 0), (2, 2), (3, 0), (3, 1))
-    assertEquals(expected1, availableCellsSoldier1)
+    val soldier = createEntity("soldier", Faction.AI, Role.Soldier)
+    grid.setCell(soldier, (2, 1))
+    val availableCellsSoldier = grid.getAdjacentAvailableCells(soldier)
+    val expected = Set((1, 0), (1, 1), (2, 0), (2, 2), (3, 0), (3, 1))
+    assertEquals(expected, availableCellsSoldier)
 
   @Test def testGetAdjacentCellsOddRow(): Unit =
-    grid.setCell("soldier", (3, 2))
-    val availableCellsSoldier3 = grid.getAdjacentAvailableCells("soldier")
+    val soldier = createEntity("soldier", Faction.AI, Role.Soldier)
+    grid.setCell(soldier, (3, 2))
+    val availableCellsSoldier3 = grid.getAdjacentAvailableCells(soldier)
     val expected3 = Set((2, 2), (2, 3), (3, 1), (3, 3), (4, 2), (4, 3))
     assertEquals(expected3, availableCellsSoldier3)
 
   @Test def testGetAdjacentCellsWithOccupied(): Unit =
-    grid.setCell("soldier", (2, 1))
-    grid.setCell("enemy", (2, 2))
-    val availableCellsSoldierWithNeighbour = grid.getAdjacentAvailableCells("soldier")
+    val soldier = createEntity("soldier", Faction.AI, Role.Soldier)
+    val wizard = createEntity("wizard", Faction.AI, Role.Mage)
+    grid.setCell(soldier, (2, 1))
+    grid.setCell(wizard, (2, 2))
+    val availableCellsSoldierWithNeighbour = grid.getAdjacentAvailableCells(soldier)
     val expectedWithOccupied = Set((1, 0), (1, 1), (2, 0), (3, 0), (3, 1))
     assertEquals(expectedWithOccupied, availableCellsSoldierWithNeighbour)
