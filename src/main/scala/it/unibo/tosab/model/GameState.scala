09baf0e8b7228be22e0fc797ca0b20df0010d6ad
lzanetti <lorenzo.zanetti5@studio.unibo.it>
2026-04-10 12:14:06 +0200

    test: GameState util methods

--- src/main/scala/it/unibo/tosab/model/GameState.scala
+++ src/main/scala/it/unibo/tosab/model/GameState.scala
@@ -1,12 +1,15 @@
 package it.unibo.tosab.model
 
 import it.unibo.tosab.model.entities.Character
-import it.unibo.tosab.model.grid.Grid
+import it.unibo.tosab.model.grid.{Coordinate, Grid}
 
 enum GamePhase:
   case Setup, Combat, GameOver
 
-case class GameState(phase: GamePhase, grid: Grid, turnQueue: Seq[String] = Seq.empty)
+case class GameState(phase: GamePhase, grid: Grid, turnQueue: Seq[String] = Seq.empty):
+  def getCharacterById(id: String): Option[Character] = ???
+
+  def getPositionOf(id: String): Option[Coordinate] = ???
 
 object GameState:
   def apply(grid: Grid): GameState = GameState(GamePhase.Setup, grid)
