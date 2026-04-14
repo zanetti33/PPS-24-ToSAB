470a79ef9a0bd42ee692c49b6018c44835db019b
lzanetti <lorenzo.zanetti5@studio.unibo.it>
2026-04-14 14:37:05 +0200

    chore: unused imports

--- src/main/scala/it/unibo/tosab/model/ai/Pathfinder.scala
+++ src/main/scala/it/unibo/tosab/model/ai/Pathfinder.scala
@@ -3,8 +3,6 @@ package it.unibo.tosab.model.ai
 import it.unibo.tosab.model.entities.Entity
 import it.unibo.tosab.model.grid.{Coordinate, Grid}
 
-import scala.collection.immutable.{AbstractSet, SortedSet}
-
 object Pathfinder:
   /** Evaluates adjacent cells and picks the one closest to the target. This is a "Greedy" approach
     */
