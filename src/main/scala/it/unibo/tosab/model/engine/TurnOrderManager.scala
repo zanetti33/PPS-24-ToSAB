ffc78937cca98d51c61de90e210435d04b711c9d
lzanetti <lorenzo.zanetti5@studio.unibo.it>
2026-04-14 17:16:15 +0200

    feat: added CombatStateTransitions for generic reusable engine functions to transit between States

--- src/main/scala/it/unibo/tosab/model/engine/TurnOrderManager.scala
+++ src/main/scala/it/unibo/tosab/model/engine/TurnOrderManager.scala
@@ -1,12 +1,22 @@
 package it.unibo.tosab.model.engine
 
 import it.unibo.tosab.model.GameState
+import it.unibo.tosab.model.entities.CombatRules.isAlive
 import it.unibo.tosab.model.entities.Character
 
 object TurnOrderManager:
+  private def livingCharacterIds(gameState: GameState): Set[String] =
+    gameState.grid.allEntities
+      .collect { case character: Character if character.isAlive => character.id }
+      .toSet
+
+  def synchronizeTurnQueue(gameState: GameState): Seq[String] =
+    val aliveIds = livingCharacterIds(gameState)
+    gameState.turnQueue.filter(aliveIds)
+
   def determineTurnOrder(gameState: GameState): Seq[String] =
     gameState.grid.allEntitiesWithPositions
-      .collect { case (character: Character, pos) => (character, pos) }
+      .collect { case (character: Character, pos) if character.isAlive => (character, pos) }
       .sortBy { case (character, (row, column)) =>
         (
           -character.stats.speed,
