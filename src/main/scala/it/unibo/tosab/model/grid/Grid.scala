4310879c20619c3ae2bdb7c013c88b41727e00ee
Alessandro Zanzi <alessandro.zanzi2@studio.unibo.it>
2026-04-24 16:41:08 +0200

    feat: implemented sight check if there are obstacles in between two troops

--- src/main/scala/it/unibo/tosab/model/grid/Grid.scala
+++ src/main/scala/it/unibo/tosab/model/grid/Grid.scala
@@ -1,6 +1,6 @@
 package it.unibo.tosab.model.grid
 
-import it.unibo.tosab.model.entities.{Entity, EntityId}
+import it.unibo.tosab.model.entities.{Entity, EntityId, Obstacle}
 
 case class Grid(size: Int = 8, cells: Map[Coordinate, Entity] = Map.empty):
   private val hexGrid = HexagonalGrid(size)
@@ -25,6 +25,31 @@ case class Grid(size: Int = 8, cells: Map[Coordinate, Entity] = Map.empty):
 
   def getDistance(p1: Coordinate, p2: Coordinate): Int =
     hexGrid.getDistance(p1, p2)
+
+  private def isInLine(from: Coordinate, to: Coordinate): Boolean =
+    if from == to then true
+    else
+      val dx = to.x - from.x
+      val dy = to.y - from.y
+
+      dx == 0 || hexGrid.getDiagonal(from, to).contains(to)
+
+  private def cellsInBetween(from: Coordinate, to: Coordinate): Set[Coordinate] =
+    if from.x == to.x then (from.y until to.y).map(y => Coordinate(from.x, y)).toSet
+    else hexGrid.getDiagonal(from, to)
+
+  def isLineOfSightClear(from: Coordinate, to: Coordinate): Boolean =
+    if !isInLine(from, to) then true
+    else
+      val blockingObstacles = getOccupiedCells
+        .intersect(cellsInBetween(from, to))
+        .filter(pos =>
+          getEntity(pos).exists {
+            case o: Obstacle => o.blocksVision
+            case _           => false
+          }
+        )
+      blockingObstacles.isEmpty
 
   def isWithinBounds(position: Coordinate): Boolean =
     hexGrid.isWithinBounds(position)
--- src/main/scala/it/unibo/tosab/model/grid/GridManager.scala
+++ src/main/scala/it/unibo/tosab/model/grid/GridManager.scala
@@ -4,6 +4,7 @@ trait GridManager:
   def size: Int
   def getDistance(p1: Coordinate, p2: Coordinate): Int
   def getNeighbors(pos: Coordinate): Set[Coordinate]
+  def getDiagonal(from: Coordinate, to: Coordinate): Set[Coordinate]
   def isWithinBounds(pos: Coordinate): Boolean =
     pos.x >= 0 && pos.x < size && pos.y >= 0 && pos.y < size
 
@@ -24,3 +25,22 @@ class HexagonalGrid(val size: Int) extends GridManager:
       else Set((-1, 0), (-1, 1), (0, -1), (0, 1), (1, 0), (1, 1))
 
     offsets.map((dx, dy) => Coordinate(x + dx, y + dy)).filter(isWithinBounds)
+
+  override def getDiagonal(from: Coordinate, to: Coordinate): Set[Coordinate] =
+    val down = from.x < to.x // se true scendo
+    val right = from.y < to.y // se true vado verso destra
+    def deltaX(x: Int): Int =
+      if down then x + 1 else x - 1
+    def deltaY(x: Int, y: Int): Int =
+      if right then if x % 2 == 0 then y else y + 1
+      else if x % 2 == 0 then y - 1
+      else y
+    var cellsInDiagonal = Set[Coordinate](from)
+    var x = from.x
+    var y = from.y
+    while x != to.x do
+      y = deltaY(x, y)
+      x = deltaX(x)
+      val cell = Coordinate(x, y)
+      if isWithinBounds(cell) then cellsInDiagonal = cellsInDiagonal + cell
+    cellsInDiagonal
