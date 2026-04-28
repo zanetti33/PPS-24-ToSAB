1c09c69cfb51d3824c351aa8323410017d910639
Fabio Pedrini <fabio.pedrini3@studio.unibo.it>
2026-04-28 12:40:48 +0200

    refactor: replace MonadIO with IO for improved functionality

--- src/main/scala/it/unibo/tosab/view/LoggerUtils.scala
+++ src/main/scala/it/unibo/tosab/view/LoggerUtils.scala
@@ -1,9 +1,8 @@
 package it.unibo.tosab.view
 
-import it.unibo.tosab.model.io.MonadIO
 import it.unibo.tosab.view.ActionLog
 
 object LoggerUtils:
 
-  def logAndDisplay(log: ActionLog): MonadIO[Unit] =
-    MonadIO.printLine(log.toString).flatMap(_ => MonadIO.sleep(3000))
+  def logAndDisplay(log: ActionLog): IO[Unit] =
+    IO.printLine(log.toString).flatMap(_ => IO.sleep(3000))
